ce126627d663205ea7016dacb2ce9436e16f2e41
Abdulaziz <wwxeww1wwxeww@gmail.com>
2024-08-20 16:06:12 +0500

    created

---
+++ src/main/java/uz/pdp/apptelegrammanagergroupbot/AppTelegramManagerGroupBotApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.apptelegrammanagergroupbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTelegramManagerGroupBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTelegramManagerGroupBotApplication.class, args);
+    }
+
+}
